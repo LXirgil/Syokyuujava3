be54ad6873ccc04a15ddc948d35c68cc7ebea842
LXirgil <hikkyhachiman0920@gmail.com>
2024-05-13 11:57:11 +0900

    初級Java2.5

---
+++ 初級Java2.5/src/ennsyuu.java
@@ -0,0 +1,10 @@
+
+public class ennsyuu {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x=2,y=3;
+		System.out.println(x*y);
+	}
+
+}
---
+++ 初級Java2.5/src/start.java
@@ -0,0 +1,12 @@
+
+public class start {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		MyFrame frame2=new RectFrame();
+		frame2.setLocation(150,200);
+		MyFrame frame3=new RectFrame();
+		frame3.setLocation(50,300);
+	}
+
+}
