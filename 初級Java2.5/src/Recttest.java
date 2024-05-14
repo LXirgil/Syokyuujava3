27edbc8de8664d837e4054fce872a4e39eae2efd
LXirgil <hikkyhachiman0920@gmail.com>
2024-05-14 12:10:56 +0900

    3.3色を付ける

--- 初級Java2.5/src/Recttest.java
+++ 初級Java2.5/src/Recttest.java
@@ -1,10 +1,20 @@
 
 public class Recttest extends MyFrame{
 	public void run() {
+		setColor(255,255,0);
 		fillRect(30,40,50,100);
+		
+		setColor(123,0,123);
 		fillRect(130,40,50,100);
+		
+		setColor(0,0,0);
 		fillRect(30,200,150,10);
-		fillRect(90,160,30,30);
+		
+		setColor(0,255,255);
+		fillRect(250,230,130,10);
+		
+		setColor(123,123,123);
+		fillRect(30,250,150,10);
 		}
 
 }
