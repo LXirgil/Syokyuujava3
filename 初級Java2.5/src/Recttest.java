665886b76b9e7df9d17f9ff39952ac3c86f357f1
LXirgil <hikkyhachiman0920@gmail.com>
2024-05-14 12:17:36 +0900

    3.4楕円

--- 初級Java2.5/src/Recttest.java
+++ 初級Java2.5/src/Recttest.java
@@ -1,18 +1,19 @@
 
 public class Recttest extends MyFrame{
 	public void run() {
+		//黄
 		setColor(255,255,0);
 		fillRect(30,40,50,100);
-		
+		//紫
 		setColor(123,0,123);
 		fillRect(130,40,50,100);
-		
+		//黒
 		setColor(0,0,0);
 		fillRect(30,200,150,10);
-		
+		//水色
 		setColor(0,255,255);
 		fillRect(250,230,130,10);
-		
+		//灰色
 		setColor(123,123,123);
 		fillRect(30,250,150,10);
 		}
