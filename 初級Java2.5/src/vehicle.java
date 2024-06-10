dd9fb2c530f627bb38bb404350b706677206446f
LXirgil <hikkyhachiman0920@gmail.com>
2024-06-10 10:43:23 +0900

    8.3　継承

---
+++ 初級Java2.5/src/vehicle.java
@@ -0,0 +1,14 @@
+
+public class vehicle {
+	int x,y,vx,vy;
+	public vehicle(int x,int y,int vx,int vy) {
+		this.x=x;
+		this.y=y;
+		this.vx=vx;
+		this.vy=vy;
+	}
+	public void move() {
+		x+=vx;
+		y+=vy;
+	}
+}
